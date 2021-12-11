63b57a3369728c2d271fa8da917ed8d1fc2244cd
dan murphy <djnmurphy@hotmail.co.uk>
2021-12-11 18:25:06 +1100

    render user info

--- client/src/components/Stories.jsx
+++ client/src/components/Stories.jsx
@@ -1,12 +1,36 @@
+import axios from 'axios';
+import React, {useEffect, useState} from 'react'
 
 
 export function Stories() {
+    const [story, setStory] = useState([])
+
+    useEffect(() => {
+        axios.get('/api/users').then(res => {
+            setStory(res.data)
+            console.log(res.data)
+            
+        })
+    }, [])
 
     
+    
+
 
     return (
+        
         <div className="content-area">
             <h1>Story Page</h1>
+                    {story.map((stor, index)=>
+                    <div key={index}>
+                        <p>{stor.name}</p>
+                    {stor.stories.map((s, index)=> {
+                       return <div key={index}>
+                            <p>{s.title}</p>
+                            <p>{s.body}</p>
+                        </div>
+                    })}    
+                    </div>)}
         </div>
     )
 }
